34039ff2b801b594b16ac70cb2dc2c1efff20db5
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2021-09-07 02:17:36 +0500

    functions added

--- 02_js_fundamentals/app.js
+++ 02_js_fundamentals/app.js
@@ -1,8 +1,28 @@
 'use strict';
 
-const myName = 'Komil';
+/* const myName = 'Komil';
 let mySurname = false;
 
 // if (myName) mySurame = true; // boommmmmmm !!!!!!! error
 if (myName) mySurname = true; // yessss !!!!! true it's working
-console.log(mySurname); // true
+console.log(mySurname); // true */
+
+// FUNCTIONS
+// INTRO
+
+/* function myName() {
+  console.log('My name is Komil');
+}
+
+// calling / running / invoking function
+
+myName();
+myName();
+myName(); */
+
+function myCars(bmw, mercedes) {
+  return `I have ${bmw} BMW and ${mercedes} Mercedes-Benz, Are you?`;
+}
+
+const myNewCars = myCars(2, 5);
+console.log(myNewCars);
