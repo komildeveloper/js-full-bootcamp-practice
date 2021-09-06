0024a44e5c9b6670b0b92f5f112943eb715d0e1b
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2021-09-07 01:47:43 +0500

    strict mode

---
+++ 02_js_fundamentals/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const myName = 'Komil';
+let mySurname = false;
+
+// if (myName) mySurame = true; // boommmmmmm !!!!!!! error
+if (myName) mySurname = true; // yessss !!!!! true it's working
+console.log(mySurname); // true
